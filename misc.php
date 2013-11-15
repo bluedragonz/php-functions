2c41ac21802e8dab43539371585687f41fa84f81
bluedragonz <brian@seoette.com>
2013-11-15 02:33:34 -0500

    Update misc.php

--- misc.php
+++ misc.php
@@ -15,6 +15,23 @@ function dirList ($directory) {
     return $results;
 }
 
+public function create_guid($namespace = '') {    
+    static $guid = '';
+    $uid = uniqid("", true);
+    $data = $namespace;
+    $data .= $_SERVER['REQUEST_TIME'];
+    $data .= $_SERVER['HTTP_USER_AGENT'];
+    $data .= $_SERVER['REMOTE_ADDR'];
+    $data .= $_SERVER['REMOTE_PORT'];
+    $hash = strtoupper(hash('ripemd128', $uid . $guid . md5($data)));
+    $guid = substr($hash,  0,  8) .
+            substr($hash,  8,  4) .
+            substr($hash, 12,  4) .
+            substr($hash, 16,  4) .
+            substr($hash, 20, 12);
+    return $guid;
+}
+	  
 if (!function_exists('object_to_array')) {
  function object_to_array($object) {
   if (is_object($object)) {
