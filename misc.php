77f0f54366e623209098a867d3c24dfe019f5f26
bluedragonz <brian@seoette.com>
2013-11-15 02:08:44 -0500

    Update misc.php

--- misc.php
+++ misc.php
@@ -15,4 +15,19 @@ function dirList ($directory) {
     return $results;
 }
 
+if (!function_exists('object_to_array')) {
+ function object_to_array($object) {
+  if (is_object($object)) {
+   $object = get_object_vars($object);
+  }
+  return (is_array($object)) ? array_map(__FUNCTION__, $object) : $object;
+ }
+}
+
+if (!function_exists('array_to_object')) {
+function array_to_object($array) {
+  return (is_array($array)) ? (object) array_map(__FUNCTION__, $array) : $array;
+ }
+}
+
 ?>
