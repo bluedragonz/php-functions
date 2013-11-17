836a2598d45389fe1cee92b26cd55ff9e39e3f55
bluedragonz <brian@seoette.com>
2013-11-17 11:38:32 -0500

    Update crawl.php

--- crawl.php
+++ crawl.php
@@ -1,15 +1,25 @@
-function getPage($url, $referer, $agent, $header, $timeout) {
+function getPage($url, $referer, $header, $timeout) {
     $ch = curl_init();
-    $ips = array("", "", "");
+    
+    $ips = array("127.0.0.1", "127.0.0.2", "127.0.0.3");
     $rand = rand(0,count($ips)-1);
     $ip = $ips[$rand];
+    
+    $useragent = array();
+    $useragent[] = 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)';
+    $useragent[] = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0';
+    $useragent[] = 'Mozilla/4.0 (compatible; MSIE 6.1; Windows XP; .NET CLR 1.1.4322; .NET CLR 2.0.50727)';
+    $useragent[] = 'Mozilla/5.0 (X11; U; Linux i686 (x86_64); en-US; rv:1.8.1.14) Gecko/20080404 Firefox/2.0.0.14';
+    $useragent[] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:27.0) Gecko/20100101 Firefox/27.0';
+    
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_HEADER, $header);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
     curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
     curl_setopt($ch, CURLOPT_REFERER, $referer);
-    curl_setopt($ch, CURLOPT_USERAGENT, $agent);
+    curl_setopt($ch, CURLOPT_USERAGENT, $useragent);
+    curl_setopt($ch, CURLOPT_INTERFACE, $ip);
     curl_setopt($ch, CURLOPT_FRESH_CONNECT, 1);
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
     curl_setopt($ch, CURLOPT_MAXREDIRS, 5);
